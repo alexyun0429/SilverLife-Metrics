0580b215026c377f044f676711c1dbf35d9bdbfc
JaeWon Yun <alexyun0429@gmail.com>
2023-09-15 21:42:45 +1000

    updated timestampConverter and database.py

--- backend/databaseManip/database.py
+++ backend/databaseManip/database.py
@@ -13,5 +13,18 @@ def addData(data):
     cursor.execute(''' INSERT INTO info_table VALUES(%s,%s)''',(name,age))
     mysql.connection.commit()
     cursor.close()
- 
-mysql = MySQL(app)
+# # this is the code for my timestampConverter.py
+# for record in data:
+#     add_stress = ("INSERT INTO stress "
+#                   "(user_access_token, local_date, local_time, stress_level_value, stress_status) "
+#                   "VALUES (%s, %s, %s, %s, %s)")
+#     cursor.execute(add_stress, (record['User Access Token'], record['Local Date'], record['Local Time'], record['Stress Level Value'], record['Stress Status']))
+
+def retrieveData_test():
+    cursor = mysql.connection.cursor()
+    a = cursor.execute("""SELECT * FROM patients""")
+    print(a.fetchall())
+
+#mysql = MySQL(app)
+#retrieveData_test()
+
--- backend/databaseManip/timestampConverter.py
+++ backend/databaseManip/timestampConverter.py
@@ -2,18 +2,15 @@ import json
 import datetime
 import pytz
 
-data = """
+def convertDataTime (data):
+    # Load the data
+    data = json.loads(data)
 
-"""
+    stress_details = data['stressDetails']
 
-# Load the data
-data = json.loads(data)
+    brisbane_tz = pytz.timezone('Australia/Brisbane')
 
-stress_details = data['stressDetails']
-
-brisbane_tz = pytz.timezone('Australia/Brisbane')
-
-for detail in stress_details:
+    for detail in stress_details:
     user_access_token = detail['userAccessToken']
     start_time_in_seconds = detail['startTimeInSeconds']
     start_time_offset_in_seconds = detail['startTimeOffsetInSeconds']
@@ -21,7 +18,37 @@ for detail in stress_details:
     time_offset_stress_level_values = detail['timeOffsetStressLevelValues']
 
     for offset, stress_value in time_offset_stress_level_values.items():
+        if stress_value < 1:
+            continue
+
+        if stress_value <= 25:
+            stress_status = 'Rest'
+        elif stress_value <= 50:
+            stress_status = 'Low'
+        elif stress_value <= 75:
+            stress_status = 'Medium'
+        else:
+            stress_status = 'High'
+
         local_date_time = datetime.datetime.fromtimestamp(start_time_in_seconds + start_time_offset_in_seconds + int(offset), brisbane_tz)
 
-        print(f"Local Date Time: {local_date_time}, Stress Level Value: {stress_value}")
-        print(f"User Access Token: {user_access_token}")
+        # wanted to name this as just "date" & "time" but it's preoccupied name in sql
+        # so named as "local_***"
+        local_date = local_date_time.date()
+        local_time = local_date_time.time()
+
+        # print(f"Local Date: {local_date}")
+        # print(f"Local Time: {local_time}")
+        # print(f"Stress Level Value: {stress_value}")
+        # print(f"Stress Status: {stress_status}")
+        # print(f"User Access Token: {user_access_token}")
+         results.append({
+                'User Access Token': user_access_token,
+                'Local Date': local_date,
+                'Local Time': local_time,
+                'Stress Level Value': stress_value,
+                'Stress Status': stress_status
+            })
+
+    return results 
+
